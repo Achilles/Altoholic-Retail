ebef72ea02519394a58becb40b0a8fb3dff36e45
teelolws <49792789+teelolws@users.noreply.github.com>
2020-11-23 10:20:03 +1300

    LibCraftCategories update

--- Altoholic/libs/LibCraftCategories-1.0/Database/enUS.lua
+++ Altoholic/libs/LibCraftCategories-1.0/Database/enUS.lua
@@ -1134,4 +1134,5 @@ LibStub("LibCraftCategories-1.0").enUS = {
   [1133]={["Name_lang"]="Fashion Accessories: Other",["HordeName_lang"]="",["ID"]=1533,["ParentTradeSkillCategoryID"]=1478,["SkillLineID"]=2787,["OrderIndex"]=9,["Flags"]=0},
   [1134]={["Name_lang"]="Fashion Accessories: Shoulders",["HordeName_lang"]="",["ID"]=1534,["ParentTradeSkillCategoryID"]=1478,["SkillLineID"]=2787,["OrderIndex"]=7,["Flags"]=0},
   [1135]={["Name_lang"]="Fashion Accessories: Back",["HordeName_lang"]="",["ID"]=1535,["ParentTradeSkillCategoryID"]=1478,["SkillLineID"]=2787,["OrderIndex"]=8,["Flags"]=0},
+  [1136]={["Name_lang"]="Bags",["HordeName_lang"]="",["ID"]=1538,["ParentTradeSkillCategoryID"]=1395,["SkillLineID"]=197,["OrderIndex"]=20,["Flags"]=0},
 }
